7eedf2b6432ac3afe8da1a608a1673a71eec1bf0
Dhruv Chopra <dhruv.chopra@shephertz.co.in>
2014-05-26 12:46:20 +0530

    First commit

---
+++ Resilient_Chat/src/appwarp/example/Constants.java
@@ -0,0 +1,11 @@
+package appwarp.example;
+
+public class Constants {
+
+	// Sample API Key & Secret Key  
+	
+	public static String apiKey = "aa67a9f4-0f01-49c0-b";
+    public static String s2Address =  "192.168.1.13";
+    public static final String roomId = "1997286879"; // admin room id
+
+}
